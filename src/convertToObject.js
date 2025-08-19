0c10a91cb44a6245be2f592271bcfdcec6c9ee36
Volodymyr Savka <savkavoldemar1@gmail.com>
2025-08-19 20:19:11 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,26 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const answer = {};
+  const arrayString = sourceString.split(';');
+
+  for (const item of arrayString) {
+    const trimmedItem = item.trim();
+
+    if (trimmedItem !== '') {
+      if (trimmedItem.includes(':')) {
+        const firstColonIndex = trimmedItem.indexOf(':');
+        const key = trimmedItem.substring(0, firstColonIndex).trim();
+        const value = trimmedItem.substring(firstColonIndex + 1).trim();
+
+        answer[key] = value;
+      }
+    }
+  }
+
+  return answer;
 }
 
 module.exports = convertToObject;
